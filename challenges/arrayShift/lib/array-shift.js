3042631cc204a5dc0a48c76f041bd9c4c930434a
SajaSwalgah <saja.swalgah@yahoo.com>
2020-01-26 16:50:03 +0200

    adding eslint

--- challenges/arrayShift/lib/array-shift.js
+++ challenges/arrayShift/lib/array-shift.js
@@ -1,17 +1,17 @@
-  
-'use strict';
+
+
 
 function insertShiftArray(arr,val){
-  var location = Math.ceil(arr.length/2)
-  var newArr=[];
-  for (let i=0; i<location; i++){
-    newArr[i] = arr[i]
+  var location = Math.ceil(arr.length / 2);
+  var newArr = [];
+  for (let i = 0; i < location; i++){
+    newArr[i] = arr[i];
   }
   newArr[newArr.length] = val;
-  for (let i=location+1; i<arr.length+1 ;i++){
-      newArr[i] = arr[i-1]
+  for (let i = location + 1; i < arr.length + 1 ;i++){
+    newArr[i] = arr[i - 1];
   }
-  return newArr
+  return newArr;
 }
 
 
--- challenges/arrayShift/lib/remove.js
+++ challenges/arrayShift/lib/remove.js
@@ -1,17 +1,17 @@
-'use strict'
+
 
 
 function removeShiftArray(arr){
-    var location = Math.ceil(arr.length/2)
-    console.log(location)
-    var newArr=[];
-    for (let i=0; i<location; i++){
-      newArr[i] = arr[i]
-    }
-    console.log(newArr)
-    for (let i=location; i<arr.length-1 ;i++){
-        newArr[i] = arr[i+1]
-    }
-    return newArr
+  var location = Math.ceil(arr.length / 2);
+  console.log(location);
+  var newArr = [];
+  for (let i = 0; i < location; i++){
+    newArr[i] = arr[i];
   }
-  module.exports = removeShiftArray;
+  console.log(newArr);
+  for (let i = location; i < arr.length - 1 ;i++){
+    newArr[i] = arr[i + 1];
+  }
+  return newArr;
+}
+module.exports = removeShiftArray;
