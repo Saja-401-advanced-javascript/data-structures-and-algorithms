f6b2ee3b6b82777092bfec9db14058a3bfa9a1f3
SajaSwalgah <saja.swalgah@yahoo.com>
2020-01-29 03:38:47 +0200

    finishing the challenge

---
+++ challenges/llMerge2/README.md
@@ -0,0 +1,11 @@
+# Challenge Summary
+to merge two linked list together so the nodes alternate between the two lists.
+
+## Challenge Description
+To write a function called mergeLists which takes two linked lists as arguments. merge the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list. 
+
+## Approach & Efficiency
+thinking of a method to write the function in a simple way then test it for validation
+
+## Solution
+using while() then if() in order to solve the challenge and using two linked list examples to test the function.
---
+++ challenges/llMerge2/ll-merge.test.js
@@ -0,0 +1,152 @@
+'use strict'
+
+const LinkedList = require('./lib/ll-merge.js')
+
+let ll = new LinkedList();
+
+describe('Linked List', () => {
+
+    beforeEach(() => {
+        ll = new LinkedList()
+    });
+
+    it('Can successfully instantiate an empty linked list', () => {
+        expect(ll.head).toBeNull()
+    });
+
+    it('Can properly insert into the linked list', () => {
+        ll.insert(5)
+        expect(ll.head.value).toEqual(5)
+    });
+
+    it('The head property will properly point to the first node in the linked list', () => {
+        ll.insert('this is the head'); /// this is the first node in the linked list
+        ll.insert(3);
+        ll.insert(6);
+        expect(ll.head.value).toEqual('this is the head')
+    });
+
+    it('Can properly insert multiple nodes into the linked list', () => {
+        ll.insert(1); /// this is the first node in the linked list
+        ll.insert(2);
+        ll.insert(3);
+        expect(typeof ll.head.next).toMatch('object')
+    });
+
+    it('Will return true when finding a value within the linked list that exists', () => {
+        ll.insert('foo'); /// this is the first node in the linked list
+        ll.insert('fuzz');
+        ll.insert('buzz');
+        expect(ll.includes('fuzz')).toBeTruthy()
+    });
+
+    it('Will return false when searching for a value in the linked list that does not exist', () => {
+        ll.insert('foo'); /// this is the first node in the linked list
+        ll.insert('fuzz');
+        ll.insert('buzz');
+        expect(ll.includes('fuzz-buzz')).toBeFalsy()
+    });
+
+    it('Can properly return a collection of all the values that exist in the linked list', () => {
+        ll.insert('foo'); /// this is the first node in the linked list
+        ll.insert('fuzz');
+        ll.insert('buzz');
+        expect(ll.toString()).toMatch("foo -> fuzz -> buzz -> NULL")
+    });
+
+    it('can insert a node before a node located in the middle of a liked list', () => {
+        ll.insert('foo');
+        ll.insert('fuzz');
+        ll.insert('buzz');
+        ll.insertBefore('fuzz', 'newValue')
+        expect(ll.toString()).toMatch(' foo -> newValue -> fuzz -> buzz -> NULL')
+    })
+
+    it('can insert a node before the first node of a liked list', () => {
+        ll.insert('foo');
+        ll.insert('fuzz');
+        ll.insert('buzz');
+        ll.insertBefore('foo', 'newValue')
+        expect(ll.toString()).toMatch('newValue -> foo -> fuzz -> buzz -> NULL')
+    })
+
+    it('can insert a node after a node located in the middle of a liked list', () => {
+        ll.insert('foo');
+        ll.insert('fuzz');
+        ll.insert('buzz');
+        ll.insertAfter('fuzz', 'newValue')
+        expect(ll.toString()).toMatch(' foo -> fuzz -> newValue -> buzz -> NULL')
+    })
+
+    it('can insert a node after a node located at the last node of a liked list', () => {
+        ll.insert('foo');
+        ll.insert('fuzz');
+        ll.insert('buzz');
+        ll.insertAfter('buzz', 'newValue')
+        expect(ll.toString()).toMatch(' foo -> fuzz -> buzz -> newValue -> NULL')
+    })
+
+    it('tests where k is greater than the length of the linked list ', () => {
+        ll.insert(1); 
+        ll.insert(2);
+        ll.insert(3);
+        ll.insert(4);
+        ll.insert(5); 
+
+        expect(ll.kthFromEnd(7)).toMatch("exception")
+    });
+
+    it('tests where k and the length of the list are the same', () => {
+        ll.insert('a');
+        ll.insert('b');
+        ll.insert('c');
+        ll.insert('d');
+        ll.insert('f'); 
+
+        expect(ll.kthFromEnd(5)).toMatch("exception")
+    });
+
+    it('tests where k is not a positive integer ', () => {
+        ll.insert('ss');
+        ll.insert('dd');
+        ll.insert('ff');
+        ll.insert('gg');
+        ll.insert('rr'); 
+
+        expect(ll.kthFromEnd(-3)).toMatch("exception")
+    })
+
+    it('tests where the linked list is of a size 1', () => {
+        ll.insert('rr'); 
+
+        expect(ll.kthFromEnd(1)).toMatch("exception")
+    });
+
+    it('tests where k is not at the end, but somewhere in the middle of the linked list', () => {
+        ll.insert('ss');
+        ll.insert('dd');
+        ll.insert('ff');
+        ll.insert('gg');
+        ll.insert('rr'); 
+
+        expect(ll.kthFromEnd(2)).toMatch("ff")
+    })
+
+    it('test mergeLists method which takes two linkedLists and merge them togather', () => {
+        const list1 = new LinkedList();
+        list1.insert(1)
+        list1.insert(3)
+        list1.insert(2)
+
+        const list2 = new LinkedList();
+        list2.insert(5)
+        list2.insert(9)
+        list2.insert(4)
+
+        ll.mergeLists(list1, list2)
+        expect(ll.toString()).toMatch("1 -> 5 -> 3 -> 9 -> 2 -> 4 -> NULL")
+    });
+
+
+
+})
